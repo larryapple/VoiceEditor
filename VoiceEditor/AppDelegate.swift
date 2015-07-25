eb1750fe9edb525495ba89f6a3839e6a6f14d795
Larry Applegate <larry@rivergatesoftware.com>
2015-07-25 15:03:04 -0700

    Initial Commit

---
+++ VoiceEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VoiceEditor
+//
+//  Created by Larry Applegate on 7/25/15.
+//  Copyright © 2015 Rivergate Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ VoiceEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VoiceEditor
+//
+//  Created by Larry Applegate on 7/25/15.
+//  Copyright © 2015 Rivergate Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
