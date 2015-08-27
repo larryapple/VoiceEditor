7401dc28842cab180d144603bfec8161d06f145e
Larry Applegate <larry@rivergatesoftware.com>
2015-08-26 20:35:07 -0700

    Redo the play files

--- VoiceEditor/Document.swift
+++ VoiceEditor/Document.swift
@@ -27,7 +27,7 @@ class Document: NSDocument, AVAudioPlayerDelegate
 		"Play_11", "Play_12", "Play_13", "Play_14", "Play_15", "Play_16", "Play_17", "Play_18", "Play_19", "Play_20",
 		"Play_21", "Play_22", "Play_23", "Play_24", "Play_25", "Play_26", "Play_27", "Play_28", "Play_29", "Play_30",
 		"Play_31", "Play_32", "Play_33", "Play_34", "Play_35", "Play_36", "Play_37", "Play_38", "Play_39", "Play_40",
-		"Play_41",
+		"Play_41", "Play_42", "Play_43",
 		
 		"First_1", "First_2", "First_3", "First_4", "First_5", "First_6", "First_7", "First_8", "First_9", "First_10",
 		"First_11", "First_12", "First_13", "First_14", "First_15", "First_16", "First_17", "First_18", "First_19", "First_20",
